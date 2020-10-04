968c7579d6c139adead22cacfa51c5263c074bd2
haowenyang <1786599665@qq.com>
2020-10-04 14:56:41 +0800

    项目的基本创建

---
+++ mall-coupon/src/main/java/com/microservicemall/mallcoupon/controller/SpuBoundsController.java
@@ -0,0 +1,90 @@
+package com.microservicemall.mallcoupon.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservicemall.mallcoupon.entity.SpuBoundsEntity;
+import com.microservicemall.mallcoupon.service.SpuBoundsService;
+import com.microservicemall.common.utils.PageUtils;
+import com.microservicemall.common.utils.R;
+
+
+
+/**
+ * 商品spu积分设置
+ *
+ * @author yangwenhao
+ * @email yangwenhao2019@163.com
+ * @date 2020-04-28 15:48:38
+ */
+@RestController
+@RequestMapping("mallcoupon/spubounds")
+public class SpuBoundsController {
+    @Autowired
+    private SpuBoundsService spuBoundsService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    //@RequiresPermissions("mallcoupon:spubounds:list")
+    public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = spuBoundsService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    //@RequiresPermissions("mallcoupon:spubounds:info")
+    public R info(@PathVariable("id") Long id){
+		SpuBoundsEntity spuBounds = spuBoundsService.getById(id);
+
+        return R.ok().put("spuBounds", spuBounds);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    //@RequiresPermissions("mallcoupon:spubounds:save")
+    public R save(@RequestBody SpuBoundsEntity spuBounds){
+		spuBoundsService.save(spuBounds);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    //@RequiresPermissions("mallcoupon:spubounds:update")
+    public R update(@RequestBody SpuBoundsEntity spuBounds){
+		spuBoundsService.updateById(spuBounds);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    //@RequiresPermissions("mallcoupon:spubounds:delete")
+    public R delete(@RequestBody Long[] ids){
+		spuBoundsService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}
